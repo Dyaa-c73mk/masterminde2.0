688ab828113f4fd3c7a82f990cf48c5255a47bce
Dyaa-c73mk <1210789@student.roc-nijmegen.nl>
2025-01-28 20:58:45 +0100

    Update mastermind2_0.java

--- src/mastermind2_0/mastermind2_0.java
+++ src/mastermind2_0/mastermind2_0.java
@@ -7,10 +7,10 @@ public class mastermind2_0 {
 
 		System.out.println("Welcome to Mastermind! Pick four colors. You have 10 chances.");
 		System.out.println("If you get an X, it means you have the color in the right place, "
-				+ "if it's a W, it means it's at the wrong place, and none of both means the color doesn't exist.");
+				+ "if it's a W, it means it's at the wrong place, and - means the color doesn't exist.");
 
 		System.out.println("Turn on caps lock, type in one line with spaces.");
-		System.out.println("Pickable colors: R, Y, G, B, P, O");
+		System.out.println("Pickable colors: R, Y, G, B, P, O");  
 
 		game.genereren();
 		game.HetSpell();
